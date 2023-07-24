8830aaa220ed49701eabebe8f62ef82a1b7faf17
Matt Rinsch <mrinsch@myvu.vinu.edu>
2023-07-23 22:26:26 -0400

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,17 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git luciApp https://github.com/cnbbx/Openwrt-LuciApp.git' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git luci-app-temp-status https://github.com/gSpotx2f/luci-app-temp-status.git' >>feeds.conf.default
-#echo 'src-git luci-app-log https://github.com/gSpotx2f/luci-app-log.git' >>feeds.conf.default
-#echo 'src-git canmeng12 https://github.com/canmeng12/packages' >>feeds.conf.default
-#echo 'src-git luciApp https://github.com/cnbbx/Openwrt-LuciApp' >>feeds.conf.default
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git me https://github.com/gfunkmonk/openwrt-packages' >>feeds.conf.default
+
